545bec9556ce103a100f25117acfdda3bc9a912d
31kish <keishi.k@meltaba.com>
2018-02-25 03:03:26 +0900

    change service Slack to LINE

--- ushikun.rb
+++ ushikun.rb
@@ -1,8 +1,12 @@
-require "./post.rb"
+require './dust.rb'
+require './post_for_line.rb'
 
 def start
-  $post = Post.new
-  $post.recycleDay()
+  dust = Dust.new
+  recycleDay = dust.recycleDay()
+  puts("day : #{recycleDay}")
+  line = PostForLine.new
+  line.post("#{0x1F42E.chr('UTF-8')} 明日は#{recycleDay}の日だよ。忘れずに捨てよう！ ﾓ〜＞ #{0x1F404.chr('UTF-8')}")
 end
 
 start
